4ffcb82489a72b938d859115fb8faad61c88731b
Requiem98 <amedeo.ranaldi@gmail.com>
2023-11-12 14:41:03 +0100

    aggiornato da DESKTOP-AME

--- models/VGG16/vgg16.py
+++ models/VGG16/vgg16.py
@@ -25,7 +25,7 @@ class VGG16_SAFA(nn.Module):
     def __init__(self, img_size:tuple, dimension : int, *args, **kargs):
         super().__init__()
         
-        self.cnn = nn.Conv2d(3,3,1)#vgg16(weights=VGG16_Weights.DEFAULT).features
+        self.cnn = vgg16(weights=VGG16_Weights.DEFAULT).features
         self.sa = SpatialAware((img_size[0] // 32) * (img_size[1] // 32), dimension) #2*8 if downsize = 2, 4*16 if downsize = 1
          
     def forward(self, x):
