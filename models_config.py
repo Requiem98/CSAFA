0ade2965f5dd9f587103f075757195d8023f8241
Requiem98 <amedeo.ranaldi@gmail.com>
2023-11-13 14:51:05 +0100

    aggiornato da DESKTOP-AME

--- models_config.py
+++ models_config.py
@@ -474,12 +474,12 @@ RT_CGAN_v0_args = ["fit", "--model", "RT_CGAN_Wrapper",
                         
                         "--data.data_to_include", '["pano", "polar"]', 
                         "--data.downscale_factor", f"{downscale}", 
-                        "--data.batch_size", "16",
+                        "--data.batch_size", "32",
                         "--data.num_workers", "16",
                         "--data.tanh", "True",
 
-                        "--trainer.gpus", "2",
-                        "--trainer.distributed_backend", "dp",
+                        "--trainer.devices", "-1",
+                        "--trainer.strategy", "dp",
                    
                         "--trainer.max_epochs", "400",
                         "--trainer.logger.version", "0",
