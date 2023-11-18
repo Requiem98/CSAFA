7d8cb70f10fa88ea4d4a54d51b9ecc67506f85c4
Requiem98 <60062428+Requiem98@users.noreply.github.com>
2023-11-18 15:55:47 +0100

    Update vgg16.py

--- models/VGG16/vgg16.py
+++ models/VGG16/vgg16.py
@@ -57,6 +57,19 @@ class VGG16_SAFA(nn.Module):
         x = self.sa(self.cnn(x)) #(B , channels)
 
         return f.normalize(x, p=2, dim=1)
+
+class VGG16_SAFA_PCA(nn.Module):
+    def __init__(self, img_size:tuple, dimension : int, *args, **kargs):
+        super().__init__()
+        
+        self.cnn = vgg16(weights=VGG16_Weights.DEFAULT).features
+        self.sa = SpatialAware((img_size[0] // 32) * (img_size[1] // 32), dimension) #2*8 if downsize = 2, 4*16 if downsize = 1
+        self.pca = LearnablePCA(512*dimension, out_dim) 
+        
+    def forward(self, x):
+        x = self.sa(self.cnn(x)) #(B , channels)
+        x = self.pca(x)
+        return f.normalize(x, p=2, dim=1)
     
     
 class VGG16_GEM_SAFA_PCA(nn.Module):
