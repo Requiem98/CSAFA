480d081949297da294c525505a4ca4e1099f04c4
Requiem98 <60062428+Requiem98@users.noreply.github.com>
2023-11-18 15:58:45 +0100

    Update models_config.py

--- models_config.py
+++ models_config.py
@@ -655,6 +655,8 @@ available_models = {"Dummy" : Dummy_args,
                     
                     "VGG16_safa_v0" : VGG16_safa_v0_args,
                     "VGG16_safa_v1" : VGG16_safa_v1_args,
+                    "VGG16_safa_v2" : VGG16_safa_v2_args,
+                    "VGG16_safa_v3" : VGG16_safa_v3_args,
                     
                     "VGG16_gem_v0" : VGG16_gem_v0_args,
                     "VGG16_gem_v1" : VGG16_gem_v1_args,
