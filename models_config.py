32e2af6a20719c7a170477796cd4760086d35d58
Requiem98 <amedeo.ranaldi@gmail.com>
2023-12-17 01:32:12 +0100

    aggiornato da DESKTOP-AME

--- models_config.py
+++ models_config.py
@@ -335,7 +335,7 @@ VGG16_safa_v13_args = ["fit", "--model", "ModelWrapper",
                         
                         "--data.data_to_include", '["pano", "polar"]', 
                         "--data.downscale_factor", f"{downscale}", 
-                        "--data.batch_size", "32",
+                        "--data.batch_size", "16",
                         "--data.num_workers", "16",
                         
                         "--trainer.max_epochs", "300",
